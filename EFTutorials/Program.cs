03bda7823c860a3ea7b55ac9c9894e0b1f4bf465
mahikero <federicoparassarmiento@yahoo.com>
2016-09-19 16:45:29 -0400

    up

--- EFTutorials/Program.cs
+++ EFTutorials/Program.cs
@@ -10,7 +10,7 @@ namespace EFTutorials
     {
         static void Main(string[] args)
         {
-
+            /*
             var newStudent = new Student();
 
             newStudent.StudentName = "Rico";
@@ -23,6 +23,34 @@ namespace EFTutorials
             }
 
             object rico = new object();
+             */
+
+            /*
+            using (var context = new SchoolEntities())
+            {
+                var students = (from s in context.SC_Student
+                           where s.StudentName == "Student1"
+                           select s).FirstOrDefault<SC_Student>();
+
+                Console.WriteLine(students.StudentName);
+               
+                Console.Read();
+            }
+             */
+
+            using (var context = new SchoolEntities())
+            {
+                var students = context.SC_Student.SqlQuery("select * from sc_student").ToList<SC_Student>();
+
+                foreach (var student in students)
+                {
+                    Console.WriteLine(student.StudentName);
+                }
+                
+                
+
+                Console.Read();
+            }
         }
     }
 }
